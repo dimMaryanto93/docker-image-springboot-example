60dddeabd378f3222a5ab15f1ecb4cff411ed02e
dimmaryanto93 <software.dimas_m93@icloud.com>
2019-01-21 11:19:28 +0700

    create sample controller

--- src/main/java/com/maryanto/dimas/example/SpringbootDockerContainerApplication.java
+++ src/main/java/com/maryanto/dimas/example/SpringbootDockerContainerApplication.java
@@ -2,13 +2,29 @@ package com.maryanto.dimas.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
 
 @SpringBootApplication
+@RestController
 public class SpringbootDockerContainerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringbootDockerContainerApplication.class, args);
-	}
+    @GetMapping("/halo")
+    public Map<String, Object> haloWorld() {
+        Map<String, Object> map = new HashMap<>();
+        map.put("message", "Hello world!");
+        map.put("time", Timestamp.valueOf(LocalDateTime.now()));
+        return map;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootDockerContainerApplication.class, args);
+    }
 
 }
 
