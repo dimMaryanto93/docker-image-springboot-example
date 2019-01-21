e127534b3a1d01b1b6337529aa735556eef7e444
dimmaryanto93 <software.dimas_m93@icloud.com>
2019-01-21 11:16:13 +0700

    init project

---
+++ src/main/java/com/maryanto/dimas/example/SpringbootDockerContainerApplication.java
@@ -0,0 +1,14 @@
+package com.maryanto.dimas.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDockerContainerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDockerContainerApplication.class, args);
+	}
+
+}
+
